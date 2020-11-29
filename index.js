e97648d8a2679d2eeffb201f0a1bef9d2eac245f
unknown <hieu73600@st.vimaru.edu.vn>
2020-11-29 14:16:12 +0700

    change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var app = express()
-var server = app.listen(3000,()=>console.log('Server open port 3000'))
+var port = process.env.PORT || 3000;
+var server = app.listen(port,()=>console.log('Server open port 3000'))
 var io = require('socket.io')(server);
 var fs = require('fs');
 app.use(express.static('public'));
